46e4aca6c9fa9359708888d5a904a92b436e7e02
Leelakanakala09 <ksleela09072007@gmail.com>
2026-01-31 12:03:57 +0530

    Rename function and update hospital query logic

--- hospitals.py
+++ hospitals.py
@@ -1,6 +1,11 @@
-def get_nearest_hospital(emergency):
+def get_nearby_hospitals(emergency):
+    if emergency in ["Road Accident", "Heavy Bleeding", "Chest Pain", "Breathing Problem"]:
+        query = "trauma hospital near me"
+    else:
+        query = "hospital near me"
+
+    maps_url = f"https://www.google.com/maps/search/{query.replace(' ', '+')}"
+
     return {
-        "name": "Apollo Trauma & Emergency Care",
-        "distance": "2.3 km",
-        "maps": "https://www.google.com/maps/search/trauma+hospital+near+me"
+        "maps": maps_url
     }
