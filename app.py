bbe08a7ccc556a172c75be0b4f2db4d707952973
Leelakanakala09 <ksleela09072007@gmail.com>
2026-01-31 16:09:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,14 +27,6 @@ def init_state():
             st.session_state[k] = v
 
 init_state()
-# ---------------- SAFE RESET HANDLER ----------------
-if st.session_state.reset_trigger:
-    st.session_state.all_symptoms = []
-    st.session_state.ui_selected = []
-    st.session_state.voice_text = ""
-    st.session_state.pop("user_role", None)  # SAFE reset for radio
-    st.session_state.reset_trigger = False
-    st.rerun()
 
 
 # ---------------- RESET HANDLER ----------------
@@ -191,10 +183,4 @@ if os.path.exists(IMAGE_PATH):
     st.image(IMAGE_PATH, use_column_width=True)
 else:
     st.warning("⚠️ Banner image not found. (assets/goldenhour.png)")
-
-  st.divider()
-
-if st.button("🔄 Start New Emergency"):
-    st.session_state.reset_trigger = True
-    st.rerun()
 
